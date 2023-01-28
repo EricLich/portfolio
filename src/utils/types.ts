c19aaa47979455defe3ffcbf23e12dc11819c686
EricLich <erick_lich@hotmail.com>
2023-01-27 23:36:55 -0300

    add experience section

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,11 @@
 export type PageLink = {
   text: string;
   link: string;
-}
+};
+
+export type Experience = {
+  timeSpan: string;
+  company: string;
+  title: string;
+  description: string;
+};
